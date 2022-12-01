c0a14fb3ef0ebdf8a526a8641e9b99db89873c98
sapphi-red <green@sapphi.red>
2022-12-02 01:28:30 +0900

    test(npm-commands): add some tests

---
+++ packages/vitepress-plugin-npm-commands/src/node/__test__/markdownPlugin.test.ts
@@ -0,0 +1,44 @@
+import MarkdownIt from 'markdown-it'
+import { test, expect } from 'vitest'
+import { npmCommandsPlugin } from '../markdownPlugin'
+import dedent from 'ts-dedent'
+
+const setup = () => {
+  const md = MarkdownIt()
+  md.use(npmCommandsPlugin)
+  return md
+}
+
+const s = '```'
+
+test('render manual correctly', () => {
+  const md = setup()
+  const result = md.render(dedent`
+    ${s}
+    npm i vite // [!=npm npm]
+    yarn add vite // [!=npm yarn]
+    pnpm add vite // [!=npm pnpm]
+    ${s}
+  `)
+  expect(result).toMatchSnapshot()
+})
+
+test('render auto correctly', () => {
+  const md = setup()
+  const result = md.render(dedent`
+    ${s}
+    npm i // [!=npm auto]
+    ${s}
+  `)
+  expect(result).toMatchSnapshot()
+})
+
+test('escape hatch', () => {
+  const md = setup()
+  const result = md.render(dedent`
+    ${s}{----npm-disable}
+    npm i // [!=npm auto]
+    ${s}
+  `)
+  expect(result).toMatchSnapshot()
+})
