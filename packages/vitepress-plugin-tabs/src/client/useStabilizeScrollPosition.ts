24cfb5ee76bd0eceb7b70c36c144210e020e80bf
翠 / green <green@sapphi.red>
2022-12-14 07:49:26 +0900

    fix(tabs): fix ssr build not working because of document (#9)

--- packages/vitepress-plugin-tabs/src/client/useStabilizeScrollPosition.ts
+++ packages/vitepress-plugin-tabs/src/client/useStabilizeScrollPosition.ts
@@ -1,15 +1,25 @@
 import { nextTick, Ref } from 'vue'
 
+type StabilizeScrollPosition = <Args extends readonly unknown[], Return>(
+  func: (...args: Args) => Return
+) => (...args: Args) => Promise<Return>
+
 export const useStabilizeScrollPosition = (
   targetEle: Ref<HTMLElement | undefined>
 ) => {
+  if (typeof document === 'undefined') {
+    const mock: StabilizeScrollPosition =
+      f =>
+      async (...args) =>
+        f(...args)
+    return { stabilizeScrollPosition: mock }
+  }
+
   const scrollableEleVal = document.documentElement
 
-  const stabilizeScrollPosition =
-    <Args extends readonly unknown[], Return>(
-      func: (...args: Args) => Return
-    ) =>
-    async (...args: Args): Promise<Return> => {
+  const stabilizeScrollPosition: StabilizeScrollPosition =
+    func =>
+    async (...args) => {
       const result = func(...args)
       const eleVal = targetEle.value
       if (!eleVal) return result
