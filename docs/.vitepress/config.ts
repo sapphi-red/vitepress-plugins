baca539d703dd35c2478933ab8c937b9ff5539aa
翠 / green <green@sapphi.red>
2023-02-06 05:15:05 +0900

    docs: add search

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -9,6 +9,11 @@ export default defineConfig({
   title: 'Vitepress Plugins',
   description: "sapphi-red's vitepress plugins",
   themeConfig: {
+    algolia: {
+      appId: 'KQMH99B171',
+      appKey: '520ec1c8df02a3b0d2aaca23cd55cac6',
+      indexName: 'vitepress-plugins-sapphi'
+    },
     editLink: {
       pattern:
         'https://github.com/sapphi-red/vitepress-plugins/edit/main/docs/:path',
