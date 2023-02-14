4a75d9622779526193b16e50f46b7d440fdae4c9
sapphi-red <green@sapphi.red>
2023-02-14 20:16:39 +0900

    chore: revert "docs: update search"
    This reverts commit 7ae79cd8d423c25ab2fef5ff4ac6c28a9082b0c6.

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -10,8 +10,8 @@ export default defineConfig({
   description: "sapphi-red's vitepress plugins",
   themeConfig: {
     algolia: {
-      appId: 'ACQGK68BKU',
-      appKey: 'b4c79549aa55db252d8676caf63e8f0d',
+      appId: 'KQMH99B171',
+      appKey: '520ec1c8df02a3b0d2aaca23cd55cac6',
       indexName: 'vitepress-plugins-sapphi'
     },
     editLink: {
