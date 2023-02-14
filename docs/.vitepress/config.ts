9919e5933b0ba2c7299f2ba604be0a964d69b3aa
sapphi-red <green@sapphi.red>
2023-02-14 20:23:54 +0900

    docs: fix doc search key

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
   themeConfig: {
     algolia: {
       appId: 'KQMH99B171',
-      appKey: '520ec1c8df02a3b0d2aaca23cd55cac6',
+      apiKey: '520ec1c8df02a3b0d2aaca23cd55cac6',
       indexName: 'vitepress-plugins-sapphi'
     },
     editLink: {
