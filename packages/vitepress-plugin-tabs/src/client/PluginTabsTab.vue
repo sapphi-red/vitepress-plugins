741a7b53243a8ff27d85ce9892f1f5b50c94cc54
sapphi-red <green@sapphi.red>
2023-09-18 16:02:05 +0900

    fix: tweak

--- packages/vitepress-plugin-tabs/src/client/PluginTabsTab.vue
+++ packages/vitepress-plugin-tabs/src/client/PluginTabsTab.vue
@@ -24,15 +24,20 @@ const { uid, selected } = useTabsSingleState()
   padding: 16px;
 }
 
-.plugin-tabs--content > :first-child {
+.plugin-tabs--content > :first-child:first-child {
   margin-top: 0;
 }
 
-.plugin-tabs--content > :last-child {
+.plugin-tabs--content > :last-child:last-child {
   margin-bottom: 0;
 }
 
-.plugin-tabs--content > div[class*='language-'] {
-  margin: 8px 0;
+.plugin-tabs--content > :deep(div[class*='language-']) {
+  border-radius: 8px;
+  margin: 16px 0px;
+}
+
+:root:not(.dark) .plugin-tabs--content :deep(div[class*='language-']) {
+  background-color: var(--vp-c-bg);
 }
 </style>
