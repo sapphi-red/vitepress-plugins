ef1b66196dc72b74a81fdc85ca563ca488c945ee
翠 <green@sapphi.red>
2025-09-14 04:00:18 +0900

    feat: precompile client codes (#85)

---
+++ .changeset/cold-spiders-joke.md
@@ -0,0 +1,5 @@
+---
+'vitepress-plugin-tabs': patch
+---
+
+Precompiled client codes so that less work is needed when running the dev server or build. It also help avoiding typescript errors.
--- packages/vitepress-plugin-tabs/tsdown.config.ts
+++ packages/vitepress-plugin-tabs/tsdown.config.ts
@@ -1,8 +1,22 @@
 import { defineConfig } from 'tsdown'
+import vue from 'unplugin-vue/rolldown'
 
-export default defineConfig({
-  entry: ['src/node/index.ts'],
-  format: 'esm',
-  dts: true,
-  target: 'node18'
-})
+export default defineConfig([
+  {
+    entry: ['src/node/index.ts'],
+    format: 'esm',
+    dts: true,
+    target: 'node18',
+    outDir: 'dist/node'
+  },
+  {
+    entry: ['src/client/index.ts'],
+    format: 'esm',
+    dts: { vue: true },
+    plugins: [vue({ isProduction: true })],
+    outDir: 'dist/client',
+    outputOptions: {
+      banner: 'import "./index.css"'
+    }
+  }
+])
